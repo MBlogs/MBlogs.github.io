ae6c128468285466dc2f6be774644dfcc6eb510c
mbrooksdw <mbrooks@darwinium.com>
2022-06-21 11:18:30 +0100

    Added Form submit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
 import React, {Component} from 'react'
 import Table from './Table.js'
+import Form from './Form.js'
 
 class App extends Component {
-  render() {
-    // Pretend this is coming from somwhere in eg. JSON format.
-    const characters = [
-        {
-          name: 'Charlie',
-          job: 'Janitor',
-        },
-        {
-          name: 'Mac',
-          job: 'Bouncer',
-        },
-        {
-          name: 'Dee',
-          job: 'Aspring actress',
-        },
-        {
-          name: 'Dennis',
-          job: 'Bartender',
-        },
-      ]
+  state = {
+    characters: [],
+  }
 
-      return (
-        <div className="container">
-          <Table characterData={characters} />
-        </div>
-      )
+  removeCharacter = (index) => {
+    // ES6 method to retrieve state.
+    const {characters} = this.state
+
+    // this.SetState built in method for manipulating state.
+    this.setState({
+      // Filter array based on index and return new array. returns all but i
+      characters: characters.filter((character, i) => {
+        return i != index
+      }),
+    })
+  }
+
+  handleSubmit = (character) => {
+    // The ... is Spread syntax. Similar to Python myList**
+    this.setState({characters: [...this.state.characters, character]})
+  }
+
+  render() {
+    const {characters} = this.state
+    
+    // Pass through the state and function to remove rows from state.
+    return (
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+        <Form handleSubmit = {this.handleSubmit}/>
+      </div>
+    )
   }
 }
 
