3389efb031001f9140e88d8a57bc2b8ecc874c7d
Michael Brooks <mjbrooks95@gmail.com>
2022-06-21 19:16:19 +0100

    Changes to App

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import './index.css'
 
-import App from './Api'
+import App from './App'
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
